64fcec7a9281f3f402aeafa166c3006c521b408e
Santiago <santiagocardenasmontes@gmail.com>
2026-06-12 17:12:57 -0500

    fix(terraform): skip_provider_registration en Azure y cambiar region a eastus2

--- infra/terraform/environments/azure/providers.tf
+++ infra/terraform/environments/azure/providers.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
   # Autenticar con: az login
   # O credenciales de service principal via variables de entorno:
   # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
--- infra/terraform/environments/azure/terraform.tfvars
+++ infra/terraform/environments/azure/terraform.tfvars
@@ -1,4 +1,4 @@
-location            = "eastus"
+location            = "eastus2"
 cluster_name        = "circleguard-aks"
 resource_group_name = "circleguard-rg"
 node_vm_size        = "Standard_B2s"
